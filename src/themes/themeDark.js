ddcf8e2ed6656a9b1c809b4bd031339ef64f1a4a
pexugadepollo <bso080601@gmail.com>
2020-04-25 00:09:12 +0200

    Initial commit

---
+++ src/themes/themeDark.js
@@ -0,0 +1,9 @@
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const themeDark = createMuiTheme({
+    palette: {
+        type: 'dark'
+    }
+});
+
+export default themeDark;
