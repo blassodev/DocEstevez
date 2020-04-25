f2b07b707f793aaed890aeb897f37ab971e65de2
pexugadepollo <bso080601@gmail.com>
2020-04-25 13:05:06 +0200

    add ClientManager page

---
+++ src/pages/ClientManager/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {DarkContainer} from '../../styles/DarkContainer'
+
+function ClientManager(props) {
+  return (
+      <DarkContainer>
+
+      </DarkContainer>
+  );
+}
+
+export default ClientManager;
