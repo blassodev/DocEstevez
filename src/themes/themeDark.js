666dd236bfda185436fafcaadbf88c9603b0e930
pexugadepollo <bso080601@gmail.com>
2020-04-25 11:29:53 +0200

    Update dark theme configuration

--- src/themes/themeDark.js
+++ src/themes/themeDark.js
@@ -3,6 +3,16 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 const themeDark = createMuiTheme({
     palette: {
         type: 'dark'
+    },
+    typography: {
+        fontFamily: "San Francisco",
+        allVariants:{
+            color: '#fff'
+        },
+        h1: {
+            fontWeight: 100
+        },
+
     }
 });
 
