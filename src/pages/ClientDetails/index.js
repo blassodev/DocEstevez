d7eedfad7d2fa4c9fd49a29397cafa87bac19d7f
pexugadepollo <bso080601@gmail.com>
2020-04-26 19:50:38 +0200

    Create ClientDetails page

---
+++ src/pages/ClientDetails/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {DarkContainer} from '../../styles/DarkContainer'
+
+function ClientDetails(){
+    return(
+        <DarkContainer>
+            
+        </DarkContainer>
+    )
+}
+
+export default ClientDetails;
