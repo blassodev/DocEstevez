e5f065f87569c953d0961c0b26331c640555462f
pexugadepollo <bso080601@gmail.com>
2020-04-30 21:01:15 +0200

    Change primary color for accessibility

--- src/themes/themeDark.js
+++ src/themes/themeDark.js
@@ -3,6 +3,9 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 const themeDark = createMuiTheme({
   palette: {
     type: "dark",
+    primary: {
+      main: "#8f79b5",
+    },
   },
   typography: {
     fontFamily: "San Francisco",
