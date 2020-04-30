8ebbf66f58fa422f98a899da573a13378d1f746f
pexugadepollo <bso080601@gmail.com>
2020-04-30 12:24:49 +0200

    delete old styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import darkTheme from "./themes/themeDark";
 import { MuiThemeProvider } from "@material-ui/core";
